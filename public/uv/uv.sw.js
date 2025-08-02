48f0fba69d5353e1e7febf1efc5a6274cf4cc363
John <playingallday383@gmail.com>
2025-08-02 14:59:42 -0400

    fix uv error

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -8,7 +8,7 @@
         for (const node of document.querySelectorAll("#uvHostname")) node.textContent = ${JSON.stringify(location.hostname)};
         reload.addEventListener("click", () => location.reload());
         uvVersion.textContent = ${JSON.stringify("3.2.10")};
-        uvBuild.textContent = ${JSON.stringify(unknown)};
+        uvBuild.textContent = ${JSON.stringify("20231023")};
     `;return`<!DOCTYPE html>
         <html>
         <head>
