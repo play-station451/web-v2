adbddb87da13880aef1b8cce80825d554dc2bd93
John <72810050+Notplayingallday383@users.noreply.github.com>
2025-08-14 18:58:24 -0400

    Revert last commit

--- server.ts
+++ server.ts
@@ -1,17 +1,17 @@
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { cors } from "hono/cors";
-import { getCookie, setCookie } from "hono/cookie";
+import express, { Response, Request } from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
+import cookieParser from "cookie-parser";
 import { server as wisp } from "@mercuryworkshop/wisp-js/server";
 import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 // @ts-expect-error types
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import Socket from "ws";
+import Head from "ws";
+import cors from "cors";
 import config from "dotenv";
 
 export function TServer() {
@@ -20,7 +20,8 @@ export function TServer() {
 	const __dirname = path.dirname(__filename);
 
 	console.log("Starting Terbium...");
-	const app = new Hono();
+	const app = express();
+	app.use(cookieParser());
 
 	const masqrCheck = process.env.MASQR && process.env.MASQR.toLowerCase() === "true";
 	if (masqrCheck) {
@@ -29,117 +30,108 @@ export function TServer() {
 		console.log(`Masqr is Disabled`);
 	}
 
-	async function MasqFail(c) {
-		const host = c.req.header("host");
-		if (!host) {
+	async function MasqFail(req, res) {
+		if (!req.headers.host) {
 			return;
 		}
-		const unsafeSuffix = host + ".html";
+		const unsafeSuffix = req.headers.host + ".html";
 		const safeSuffix = path.normalize(unsafeSuffix).replace(/^(\.\.(\/|\\|$))+/, "");
 		const safeJoin = path.join(process.cwd() + "/Masqrd", safeSuffix);
 		try {
 			await fs.promises.access(safeJoin);
 			const failureFileLocal = await fs.promises.readFile(safeJoin, "utf8");
-			return c.html(failureFileLocal);
+			res.setHeader("Content-Type", "text/html");
+			res.send(failureFileLocal);
+			return;
 		} catch (e) {
-			return c.html(fs.readFileSync("fail.html", "utf8"));
+			res.setHeader("Content-Type", "text/html");
+			res.send(fs.readFileSync("fail.html", "utf8"));
+			return;
 		}
 	}
 
 	if (masqrCheck) {
-		app.use(async (c, next) => {
-			const host = c.req.header("host");
+		app.use(async (req, res, next) => {
 			// @ts-expect-error stfu
-			if (host && process.env.WHITELISTED_DOMAINS.includes(host)) {
-				await next();
+			if (req.headers.host && process.env.WHITELISTED_DOMAINS.includes(req.headers.host)) {
+				next();
 				return;
 			}
-			if (c.req.url.includes("/bare")) {
-				await next();
+			if (req.url.includes("/bare")) {
+				next();
 				return;
 			}
-			const authheader = c.req.header("authorization");
-			if (getCookie(c, "authcheck")) {
-				await next();
+			const authheader = req.headers.authorization;
+			if (req.cookies["authcheck"]) {
+				next();
 				return;
 			}
-			if (getCookie(c, "refreshcheck") != "true") {
-				setCookie(c, "refreshcheck", "true", { maxAge: 10 });
-				return MasqFail(c);
+			if (req.cookies["refreshcheck"] != "true") {
+				res.cookie("refreshcheck", "true", { maxAge: 10000 });
+				MasqFail(req, res);
+				return;
 			}
 			if (!authheader) {
-				c.header("WWW-Authenticate", "Basic");
-				c.status(401);
-				return MasqFail(c);
+				res.setHeader("WWW-Authenticate", "Basic");
+				res.status(401);
+				MasqFail(req, res);
+				return;
 			}
 
 			const auth = Buffer.from(authheader.split(" ")[1], "base64").toString().split(":");
 			const user = auth[0];
 			const pass = auth[1];
-			const licenseCheck = (await (await fetch(process.env.LICENSE_SERVER_URL + pass + "&host=" + host)).json())["status"];
-			console.log(`\x1b[0m${process.env.LICENSE_SERVER_URL}${pass}&host=${host} ` + `returned: ${licenseCheck}`);
+			const licenseCheck = (await (await fetch(process.env.LICENSE_SERVER_URL + pass + "&host=" + req.headers.host)).json())["status"];
+			console.log(`\x1b[0m${process.env.LICENSE_SERVER_URL}${pass}&host=${req.headers.host} ` + `returned: ${licenseCheck}`);
 			if (licenseCheck == "License valid") {
-				setCookie(c, "authcheck", "true", { expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) });
-				return c.html(`<script> window.location.href = window.location.href </script>`);
+				res.cookie("authcheck", "true", { expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) });
+				res.send(`<script> window.location.href = window.location.href </script>`);
+				return;
 			}
-			return MasqFail(c);
+			MasqFail(req, res);
+			return;
 		});
 	}
 
 	app.use(
-		"*",
-		serveStatic({
-			root: "./dist",
-			onNotFound: (path, c) => {},
+		express.static("dist", {
+			setHeaders: (res, path) => {
+				if (path.endsWith(".cjs")) {
+					res.setHeader("Content-Type", "text/javascript");
+				}
+			},
 		}),
 	);
-
-	app.use("*", async (c, next) => {
-		await next();
-		if (c.req.url.endsWith(".cjs")) {
-			c.header("Content-Type", "text/javascript");
-		}
-	});
-
-	app.use("/libcurl/*", serveStatic({ root: libcurlPath }));
-	app.use("/baremux/*", serveStatic({ root: baremuxPath }));
-	app.use("/epoxy/*", serveStatic({ root: epoxyPath }));
-
+	app.use("/libcurl/", express.static(libcurlPath));
+	app.use("/baremux/", express.static(baremuxPath));
+	app.use("/epoxy/", express.static(epoxyPath));
 	wisp.options.dns_method = "resolve";
 	wisp.options.dns_servers = ["1.1.1.3", "1.0.0.3"];
 	wisp.options.dns_result_order = "ipv4first";
+	const server = createServer();
 
-	const port = parseInt(process.env.PORT || "8080");
-
-	// Configure CORS
-	app.use(
-		"/*",
-		cors({
-			origin: `http://localhost:${port}`,
-			allowMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-			credentials: true,
-		}),
-	);
-
-	const server = serve({
-		fetch: app.fetch,
-		port,
-		createServer: () => {
-			const httpServer = createServer();
-
-			httpServer.on("upgrade", (req, socket, head) => {
-				if (req.url?.endsWith("/wisp/")) {
-					wisp.routeRequest(req, socket, head);
-				}
-			});
-
-			return httpServer;
-		},
+	server.on("request", (req: Request, res: Response) => {
+		app(req, res);
 	});
 
+	server.on("upgrade", (req: Request, socket: Socket, head: Head) => {
+		if (req.url.endsWith("/wisp/")) {
+			wisp.routeRequest(req, socket, head);
+		}
+	});
+
+	const port = parseInt(process.env.PORT || "8080");
+	const corsOptions = {
+		origin: `http://localhost:${port}`,
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	};
+
+	app.use(cors(corsOptions));
 	const manifest = fs.readFileSync(path.join(__dirname, "package.json"), "utf-8");
 	const { version } = JSON.parse(manifest);
-	console.log(`
+	server.listen(port, () => {
+		console.log(`
   \x1b[38;2;50;174;98m@@@@@@@@@@@@@@~ B@@@@@@@@#G?.
   \x1b[38;2;50;174;98mB###&@@@@&####^ #@@@&PPPB@@@G.
   \x1b[38;2;50;174;98m .. ~@@@@J ..  .#@@@P   ~&@@@^      \x1b[38;2;60;195;240mWelcome to Terbium React v${version}
@@ -149,6 +141,7 @@ export function TServer() {
       \x1b[38;2;50;174;98m^@@@@?     .#@@@&GGG#@@@@Y
       \x1b[38;2;50;174;98m^&@@@?      B@@@@@@@@&B5~
     `);
+	});
 
 	process.on("SIGINT", () => {
 		console.log("\x1b[0m");
