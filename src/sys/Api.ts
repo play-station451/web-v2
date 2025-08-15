e46fdbada9e4a0bb8c80044c320cac1bdb1855d9
John <playingallday383@gmail.com>
2025-08-14 20:47:51 -0400

    temporarily disable platform api

--- src/sys/Api.ts
+++ src/sys/Api.ts
@@ -807,6 +807,7 @@ export default async function Api() {
 		},
 		platform: {
 			async getPlatform() {
+				/*
 				const mobileuas =
 					/(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|samsungbrowser.*mobile|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk|iPhone|iPad/i;
 				const crosua = /CrOS/;
@@ -816,7 +817,8 @@ export default async function Api() {
 					return "mobile";
 				} else {
 					return "desktop";
-				}
+				}*/
+				return "desktop";
 			},
 		},
 		process: {
